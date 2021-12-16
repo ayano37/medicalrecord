bfc1e479d93c67835a5d409d9a476e0e966e5430
川原彩乃 <ayn.030307@gmail.com>
2021-12-16 13:56:37 +0000

    画像登録

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
         $data = $request->all();
         //dd($request);
         if (isset($data['image'])) {
-        $path = Storage::disk('s3')->putFile('/',$data['image'],'public');    
+        $path = Storage::disk('s3')->put('/',$data['image'],'public');    
         //$path = $data['image']->store('public/image');
         $avatar_image = Storage::disk('s3')->url($path);
       } else {
